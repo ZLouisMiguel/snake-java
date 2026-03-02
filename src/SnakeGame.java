65f23a77bfa6df114f2202a870e2f9b65d5a1021
ZLouisMiguel <zigirumugabelouismiguel@gmail.com>
2026-03-02 14:03:48 +0200

    Added tile size for grid based scaling of the whole jpanel

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -5,15 +5,30 @@ import java.util.Random;
 import javax.swing.*;
 
 
-public class SnakeGame extends JPanel{
+public class SnakeGame extends JPanel {
+
+    private class Tile {
+        int x;
+        int y;
+
+        Tile(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+
     int boardWidth;
     int boardHeight;
+    int tileSize = 25;
 
-    SnakeGame(int boardWidth, int boardHeight){
+
+    SnakeGame(int boardWidth, int boardHeight) {
         this.boardWidth = boardWidth;
         this.boardHeight = boardHeight;
         setPreferredSize(new Dimension(this.boardWidth, this.boardHeight));
         setBackground(Color.black);
+
 
     }
 }
