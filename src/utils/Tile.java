399139bdac76e2fd6fa47bc6649f5824eb139c1a
ZLouisMiguel <zigirumugabelouismiguel@gmail.com>
2026-03-04 10:30:05 +0200

    Util class for tile rendering on the game board

--- src/utils/Tile.java
+++ src/utils/Tile.java
@@ -1,4 +1,14 @@
 package utils;
 
 public class Tile {
+    public int x, y;
+
+    public Tile(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public boolean equals(Tile other) {
+        return this.x == other.x && this.y == other.y;
+    }
 }
